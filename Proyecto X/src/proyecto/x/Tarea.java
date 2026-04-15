990f44a6dd776a489fcdb6c092f4c9e8f110ab68
Nadia <lagoanadia@gmail.com>
2026-04-15 23:27:30 +0200

    Version Actualizada 15/4/2026 , 23:25
    Ampliación Tareas y asignación de Tareas

--- Proyecto X/src/proyecto/x/Tarea.java
+++ Proyecto X/src/proyecto/x/Tarea.java
@@ -2,9 +2,35 @@
 package proyecto.x;
 
 public class Tarea {
-  String nombre;
+  String nombreTarea;
+  boolean asignada;
+  Usuario usuarioAsignado;
   
-  public Tarea (){
+   public Tarea (){
+      nombreTarea="Nueva Tarea";
+      asignada=false;
+      usuarioAsignado= null;
+  } 
+  public Tarea (String nombre, boolean asignada, Usuario usuarioAsignado){
+      this.nombreTarea = nombre;
+      this.asignada = asignada;
+      this.usuarioAsignado = usuarioAsignado;
+  }
+  
+  public String getNombreTarea(){
+      return nombreTarea;
+  }
+  // Si el campo usuario es null el campo asignada sera falso dando a entender que no dicha tarea aun no ha sido asignada.
+  public boolean siAsignada(Usuario usuario){
+      if( this.usuarioAsignado == null ){
+          asignada = false;
+      } else{ asignada = true;
       
+      
+  
+      }
+      return asignada;
   }
 }
+
+  
