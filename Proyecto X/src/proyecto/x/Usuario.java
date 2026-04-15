85edaed705a71bdf00cfb3cc25e2a05960450b09
Nadia <lagoanadia@gmail.com>
2026-04-15 17:20:20 +0200

    Add files via upload

---
+++ Proyecto X/src/proyecto/x/Usuario.java
@@ -0,0 +1,13 @@
+
+package proyecto.x;
+
+public class Usuario {
+    String nombre;
+    public Usuario(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombreUsuario(){
+         return nombre;
+}
+}
+    
---
+++ x/Tarea.java
@@ -0,0 +1,10 @@
+
+package proyecto.x;
+
+public class Tarea {
+  String nombre;
+  
+  public Tarea (){
+      
+  }
+}
