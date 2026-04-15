72f07a6142fc0b5774ce205a0723be7aa615b637
Nadia <lagoanadia@gmail.com>
2026-04-15 23:21:39 +0200

    Enhance Usuario class with task assignment features
    Added a default constructor and a method to assign tasks.

--- Proyecto X/src/proyecto/x/Usuario.java
+++ Proyecto X/src/proyecto/x/Usuario.java
@@ -1,13 +1,26 @@
 
 package proyecto.x;
+import java.util.ArrayList;
 
 public class Usuario {
     String nombre;
+    ArrayList<Tarea> tareaAsignada;
+    
     public Usuario(String nombre){
         this.nombre = nombre;
+    }
+   // El usuario creado por defecto no tendra ninguna tarea asignada por lo que el campo sera null 
+      public Usuario(){
+        this.nombre = "Usuario Nuevo";
+        this.tareaAsignada = null;
     }
     public String getNombreUsuario(){
          return nombre;
 }
+    //metodo para asignarle una tarea a un usuario 
+    public void setTarea(String nombre, ArrayList<Tarea> tareaAsignada){
+        this.nombre = nombre;
+        this.tareaAsignada = tareaAsignada;
+    }
 }
     
